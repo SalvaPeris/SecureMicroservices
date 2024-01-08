2dba5a654ffccb01c6ea913e92357c4770d5dc91
speris <speris@grupokh.com>
2024-01-08 13:12:28 +0100

    08.01 - Added MoviesController and MoviesContext.

---
+++ Movies.API/Data/MoviesContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.API.Models;
+
+namespace Movies.API.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext(DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
