dc523414fd737144520817c8d0c4f1be99872abb
speris <speris@grupokh.com>
2024-01-10 11:55:14 +0100

    10.01 - Added MVC Movies Client application.

---
+++ Movies.Client/ApiServices/IMovieApiService.cs
@@ -0,0 +1,13 @@
+﻿using Movies.Client.Models;
+
+namespace Movies.Client.ApiServices
+{
+    public interface IMovieApiService
+    {
+        Task<IEnumerable<Movie>> GetMovies();
+        Task<Movie> GetMovie(string id);
+        Task<Movie> CreateMovie(Movie movie);
+        Task<Movie> UpdateMovie(Movie movie);
+        Task DeleteMovie(int id);
+    }
+}
---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,38 @@
+﻿using Movies.Client.Models;
+using Newtonsoft.Json;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public MovieApiService()
+        {
+           
+        }
+
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            return new List<Movie>();
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
